61f08fce9833110e9239b330b27c8c296c0c4ec9
antoinegrandin <antoinegrandin@free.fr>
2021-02-13 19:39:23 +0100

    Add about section

--- app/src/main/java/com/example/bankaccount/AccountActivity.kt
+++ app/src/main/java/com/example/bankaccount/AccountActivity.kt
@@ -1,6 +1,10 @@
 package com.example.bankaccount
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
@@ -11,6 +15,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class AccountActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.item_about -> {
+                startActivity(Intent(this, AboutActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
