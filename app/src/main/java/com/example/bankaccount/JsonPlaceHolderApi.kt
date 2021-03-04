a7522d1d58778c0dda308d1ec05ea46ef549d32b
antoinegrandin <antoinegrandin@free.fr>
2021-03-04 10:58:20 +0100

    Hide URL and make first APK

--- app/src/main/java/com/example/bankaccount/JsonPlaceHolderApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bankaccount
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface JsonPlaceHolderApi {
-    @get:GET("accounts")
-    val accounts: Call<List<Account>>
-}
